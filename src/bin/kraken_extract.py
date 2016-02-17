4e396b9dcf6ff4d934652d8674821c979b572d42
Benjamin Hillmann <bhillmann@outlook.com>
2016-02-17 13:46:55 -0600

    Started the kraken extract script.

---
+++ src/bin/kraken_extract.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import argparse
+
+# The arg parser for this wrapper
+def make_arg_parser():
+    parser = argparse.ArgumentParser(description='Simulate a metagenomic community from DWGSIM according to a powerlaw.')
+    parser.add_argument('-i', '--input', type=str, help='The species name file. One species name per line.', required=True)
+    return parser
+
+def main(args):
+    with if args.input == "-" sys.stdin else open(args.input, 'rb') as inf:
+        for line in inf:
+            print(line)
+
+
+if __name__ == '__main__':
+    parser = make_arg_parser()
+    args = parser.parse_args()
+    main(args)
