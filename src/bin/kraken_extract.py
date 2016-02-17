081eb490ccf3e6f78d99095e5c2b10621d7660ce
Benjamin Hillmann <bhillmann@outlook.com>
2016-02-17 13:56:46 -0600

    Fixed the command line arguments.

--- src/bin/kraken_extract.py
+++ src/bin/kraken_extract.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import argparse
+import sys
 
 # The arg parser for this wrapper
 def make_arg_parser():
     parser = argparse.ArgumentParser(description='Simulate a metagenomic community from DWGSIM according to a powerlaw.')
     parser.add_argument('-i', '--input', type=str, help='The species name file. One species name per line.', required=True)
+    parser.add_argument('-o', '--output', type=str, help='The output file.')
     return parser
 
 def main(args):
     with sys.stdin if args.input == "-" else open(args.input, 'rb') as inf:
-        for line in inf:
-            print(line)
-
+        with open(args.output, 'wb') if args.output else sys.stdout as outf:
+            for line in inf:
+                outf.write(line)
 
 if __name__ == '__main__':
     parser = make_arg_parser()
