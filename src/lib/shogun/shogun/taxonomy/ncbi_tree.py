ea9ba5907aeb4dd42faced695e7c2ece70691e3b
Benjamin Hillmann <bhillmann@outlook.com>
2016-02-25 15:12:26 -0600

    Updated for easier interface

--- src/lib/shogun/shogun/taxonomy/ncbi_tree.py
+++ src/lib/shogun/shogun/taxonomy/ncbi_tree.py
@@ -21,19 +21,36 @@ import os
 import networkx as nx
 import pickle
 import gzip
+import tarfile
+import urllib.request
+import csv
+
 from shogun import SETTINGS
 
 
 class NCBITree:
-    def __init__(self, path=os.path.join(SETTINGS.data_dir, 'taxdmp')):
-        self.tree = nx.DiGraph()
+    def __init__(self, cache=True, _ncbi_taxdmp_url=SETTINGS.ncbi_taxdmp_url, _ncbi_taxdmp_path=os.path.join(SETTINGS.data_path, "ncbi_taxdmp"), _cache_path=SETTINGS.cache_path):
+            self.tree = nx.DiGraph()
+            # construct name -> taxon_id mapping
+            self.name2taxon_id = {}
+            self.taxon_id2name = {}
+            self._cache_path = _cache_path
+            self._ncbi_taxdmp_url = _ncbi_taxdmp_url
+            self._ncbi_taxdmp_path = _ncbi_taxdmp_path
 
-        # construct name -> taxon_id mapping
-        self.name2taxon_id = {}
-        self.taxon_id2name = {}
-        with open(os.path.join(path, 'names.dmp'), 'rb') as handle:
-            for line in handle:
-                cols = line.split(b'\t')
+            if not os.path.exists(self._ncbi_taxdmp_path):
+                os.makedirs(self._ncbi_taxdmp_path)
+                self._ncbi_taxdmp_url()
+
+            self._parse_taxonomy()
+
+            if cache:
+                self.save()
+
+    def _parse_taxonomy(self):
+        with open(os.path.join(self._ncbi_taxdmp_path, 'names.dmp'), 'r') as handle:
+            csv_handle = csv.reader(handle, delimiter="\t")
+            for cols in csv_handle:
                 taxon_id = cols[0]
                 name = cols[2]
                 self.name2taxon_id[name] = taxon_id
@@ -43,9 +60,9 @@ class NCBITree:
         # construct node tree
         edges = []
         nodes = {}
-        with open(os.path.join(path, 'nodes.dmp'), 'rb') as handle:
-            for line in handle:
-                cols = line.split(b'\t')
+        with open(os.path.join(self._ncbi_taxdmp_path, 'nodes.dmp'), 'r') as handle:
+            csv_handle = csv.reader(handle, delimiter="\t")
+            for cols in csv_handle:
                 parent_node = cols[2]
                 children_node = cols[0]
                 rank = cols[4]
@@ -56,23 +73,23 @@ class NCBITree:
         self.tree.add_edges_from(edges)
         nx.set_node_attributes(self.tree, 'rank', nodes)
 
-    def save(self, _cache_dir=SETTINGS.cache_dir):
-        self_dump = os.path.join(_cache_dir, "ncbi_taxon_tree.pklz")
+    def save(self):
+        self_dump = os.path.join(self._cache_path, "ncbi_taxon_tree.pklz")
         with gzip.open(self_dump, 'wb') as handle:
             pickle.dump(self, handle)
 
     @classmethod
-    def load(cls, _cache_dir=SETTINGS.cache_dir):
+    def load(cls, _cache_dir=SETTINGS.cache_path):
         try:
             self_dump = os.path.join(_cache_dir, "ncbi_taxon_tree.pklz")
             with gzip.open(self_dump, 'rb') as handle:
-                self = pickle.load(handle)
+                return pickle.load(handle)
         except FileNotFoundError as error:
-            raise
+            return NCBITree(cache=True)
 
-    def get_taxon_id_lineage_with_taxon_id(self, taxonID):
-        path = [taxonID]
-        current_node = taxonID
+    def get_taxon_id_lineage_with_taxon_id(self, taxon_id):
+        path = [taxon_id]
+        current_node = taxon_id
         if current_node not in self.tree.nodes():
             return []
         while len(self.tree.successors(current_node)) > 0:
@@ -86,8 +103,8 @@ class NCBITree:
         path = self.get_taxon_id_lineage_with_taxon_id(self.name2taxon_id[name])
         return path
 
-    def get_name_lineage_with_taxon_id(self, taxonID):
-        taxon_id_lineage = self.get_taxon_id_lineage_with_taxon_id(taxonID)
+    def get_name_lineage_with_taxon_id(self, taxon_id):
+        taxon_id_lineage = self.get_taxon_id_lineage_with_taxon_id(taxon_id)
         name_lineage = []
         for x in taxon_id_lineage:
             rank = self.tree.node[x]['rank']
@@ -117,9 +134,15 @@ class NCBITree:
             return None, None
         return self.get_rank_with_taxon_id(self.name2taxon_id[name], rank)
 
+    def _download_ncbi_dump(self):
+        req = urllib.request.Request(self._ncbi_taxdmp_url)
+        with urllib.request.urlopen(req, 'rb') as ftp_stream:
+            tfile = tarfile.open(fileobj=ftp_stream, mode='r|gz')
+            tfile.extractall(self._ncbi_taxdmp_path)
 
 def main():
-    pass
+    ncbi_tree = NCBITree.load()
+    print("Hello World!")
 
 if __name__ == '__main__':
     main()
