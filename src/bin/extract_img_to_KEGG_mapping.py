d09b990b6a59d96100cc75e530913eeb735a8c6d
Benjamin Hillmann <bhillmann@outlook.com>
2016-03-11 11:20:31 -0600

    Formatted file to meet specifications

--- src/bin/extract_img_to_KEGG_mapping.py
+++ src/bin/extract_img_to_KEGG_mapping.py
@@ -3,9 +3,9 @@
 # python extract_IMG_to_KEGG_mapping.py -i IMGDIR -o outputfile.txt
 from __future__ import print_function
 import argparse
-import csv
 import sys
 import os
+
 
 def make_arg_parser():
     parser = argparse.ArgumentParser(description='')
@@ -24,17 +24,7 @@ def read_img_ko_table(f, IMG_ID=None):
         ko_id = words[9]
         if IMG_ID is not None:
             gene_id = IMG_ID + '_' + gene_id
-        yield(gene_id, ko_id)
-
-
-def write_gene_length_counts(outf, lengths):
-    columns = ['domain', 'genus', 'species', 'length']
-
-    wr = csv.writer(outf, quoting=csv.QUOTE_ALL)
-    wr.writerow(columns)
-
-    map(wr.writerow, [list(species) + [lengths[species]] for species in lengths])
-    outf.close()
+        yield (gene_id, ko_id)
 
 
 def main():
@@ -46,20 +36,21 @@ def main():
     sys.stderr.flush()
     img_dirs = [f for f in os.listdir(args.IMG_dir) if os.path.isdir(os.path.join(args.IMG_dir, f))]
 
-    sys.stderr.write('Parsing %d directories...\n' %(len(img_dirs)))
+    sys.stderr.write('Parsing %d directories...\n' % (len(img_dirs)))
     sys.stderr.flush()
 
     with open(args.output, 'wb') if args.output else sys.stdout as outf:
         for i, imgID in enumerate(img_dirs):
             if (i + 1) % 100 == 0:
-                sys.stderr.write('Parsing folder ' + str(i+1) + ' of ' + str(len(img_dirs)) + '\n')
-                sys.stderr.flush()                
+                sys.stderr.write('Parsing folder ' + str(i + 1) + ' of ' + str(len(img_dirs)) + '\n')
+                sys.stderr.flush()
             ko_fp = os.path.join(args.IMG_dir, imgID, imgID + ".ko.tab.txt")
             if os.path.exists(ko_fp):
                 with open(ko_fp, 'rb') as inf:
                     database_gen = read_img_ko_table(inf, imgID)
                     for gene_id, ko in database_gen:
                         outf.write(gene_id + '\t' + ko + '\n')
+
 
 if __name__ == '__main__':
     main()
