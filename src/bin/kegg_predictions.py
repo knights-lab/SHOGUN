2c1661385be3051a70cadd6fbba1edc9352e7d92
Benjamin Hillmann <bhillmann@outlook.com>
2016-03-15 20:23:03 -0500

    Interface is now ready for intersection

--- src/bin/kegg_predictions.py
+++ src/bin/kegg_predictions.py
@@ -21,27 +21,50 @@ def split_iter(string, sep=''):
     return (x.group(0) for x in re.finditer(r, string))
 
 
+def intersection():
+    pass
+
+
 def main():
     parser = make_arg_parser()
     args = parser.parse_args()
 
-    # img2kegg = {}
-    # with open(args.mapping) as inf:
-    #     csv_inf = csv.reader(inf, delimiter='\t')
-    #     for line in csv_inf:
-    #         img2kegg[line[0]] = line[1]
+    img2kegg = defaultdict(set)
+    with open(args.mapping) as inf:
+        csv_inf = csv.reader(inf, delimiter='\t')
+        for row in csv_inf:
+            img2kegg[row[0][:row[0].find('_')]].add(row[1])
 
-    img2kegg = pd.read_csv(args.mapping, delimiter='\t', header=None, index_col=0, names=['img_id', 'kegg_id'])['kegg_id']
-    counts = {}
+    # img_df = pd.read_csv(args.mapping, delimiter='\t', header=None, index_col=0, names=['img_id', 'kegg_id'])
+    # img_df['img_species_id'] = [i[:i.find('_')] for i in img_df.index.values]
+    # for index, row in img_df.iterrows():
+    #     img2kegg[row['img_species_id']].add(row['kegg_id'])
+
+    counts = defaultdict(lambda: defaultdict(int))
 
     with open(args.input) as inf:
         csv_inf = csv.reader(inf)
         header = next(csv_inf)
         # 'sample_id', 'sequence_id', 'ncbi_tid', 'img_id'
         for line in csv_inf:
-            img_ids = split_iter(line[3], ',')
-            for img_id in img_ids:
-                line[:1]
+            img_ids = line[-1].split(',')
+            sample_dict = counts[line[0]]
+            if img_ids:
+                if len(img_ids) > 1:
+                    first = img_ids.pop()
+                    kegg_intersection = img2kegg[first[:first.find('_')]]
+                    for img_id in img_ids:
+                        kegg_ids = img2kegg[img_id[:img_id.find('_')]]
+                        kegg_intersection = kegg_ids.intersection(kegg_intersection)
+                else:
+                    kegg_intersection = img2kegg[img_ids[0][:img_ids[0].find('_')]]
+                for key in kegg_intersection:
+                    sample_dict[key] += 1
+
+    with open(args.output, 'wb') if args.output else sys.stdout as outf:
+        count_df = pd.DataFrame(counts)
+        outf.write(count_df.T.to_csv())
+
 
 
 if __name__ == '__main__':
