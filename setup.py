5756284f0f1b74f83d47927e5c3d6cd413f4231b
Benjamin Hillmann <bhillmann@outlook.com>
2016-05-23 13:22:31 -0500

    First test of setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
-from distutils.core import setup
+from setuptools import setup
+from glob import glob
+import os
+
+__author__ = "Knights Lab"
+__copyright__ = "Copyright (c) 2016--, %s" % __author__
+__credits__ = ["Benjamin Hillmann", "Dan Knights", "Gabe Al-Ghalith", "Tonya Ward", "Pajua Vangay"]
+__email__ = "hillmannben@gmail.com"
+__license__ = "GPL"
+__maintainer__ = "Benjamin Hillmann"
+__version__ = "0.0.1-dev"
+
+long_description = ''
 
 setup(
     name='shogun',
-    version='',
-    packages=['shogun'],
+    version=__version__,
+    packages=['ninja_shogun'],
     url='',
-    license='',
-    author='bhillmann',
-    author_email='',
-    description=''
+    license=__license__,
+    author=__author__,
+    author_email=__email__,
+    description='',
+    long_description=long_description,
+    scripts=glob(os.path.join('scripts', '*py')),
+    keywords='',
+    install_requires=[]
 )
