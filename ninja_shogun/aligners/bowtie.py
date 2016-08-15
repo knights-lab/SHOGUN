5da421b0158dbc8f1296eef400ab740fb3890fa5
Benjamin Hillmann <bhillmann@outlook.com>
2016-08-15 11:58:47 -0500

    Added the comments.

--- ninja_shogun/aligners/bowtie.py
+++ ninja_shogun/aligners/bowtie.py
@@ -4,6 +4,16 @@ from .. import SETTINGS
 
 
 def bowtie2(infile, outfile, database, alignments_to_report=32, num_threads=SETTINGS.N_jobs, shell=False):
+    """
+    Search a bowtie2 index with multiple alignment.
+    :param infile: the query FASTA file
+    :param outfile: the resulting SAM file
+    :param database: path to the bowtie2 index
+    :param alignments_to_report: the number of alignments to report (default=32)
+    :param num_threads: the number of threads to use (default=SETTINGS)
+    :param shell: whether to use the shell NOT RECOMMENDED (default=False)
+    :return: the STDERR/STDOUT
+    """
     cmd = ['bowtie2',
            '--no-unal',
            '-x', database,
@@ -23,5 +33,13 @@ def bowtie2(infile, outfile, database, alignments_to_report=32, num_threads=SETT
 
 
 def bowtie2_build(infile, outfile, offrate=3, shell=False):
+    """
+    This function will build a bowtie2 index with a given infile and output to the outfile
+    :param infile: the FASTA file to build the index with
+    :param outfile: the prefix for the bowtie2 index
+    :param offrate: offrate for the index (default=3)
+    :param shell: whether to use the shell NOT RECOMMENDED (default=False)
+    :return: teh STDERR/STDOUT
+    """
     cmd = ['bowtie2-build', '-f', '-o', str(offrate), infile, outfile]
     return run_command(cmd, shell=shell)
