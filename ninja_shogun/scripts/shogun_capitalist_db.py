ade41f1b7f32dff0586764748fe3af72252a6cfe
Benjamin Hillmann <bhillmann@outlook.com>
2016-08-17 14:11:37 -0500

    Added the capitalist db.

--- ninja_shogun/scripts/shogun_capitalist_db.py
+++ ninja_shogun/scripts/shogun_capitalist_db.py
@@ -27,7 +27,7 @@ def shogun_capitalist_db(input, output, extract_ncbi_tid):
         title.replace('\t', '|')
         ncbi_tid = int(find_between(title, begin, end))
         if ncbi_tid:
-            gg = tree.gg_lineage(ncbi_tid[0])
+            gg = tree.gg_lineage(ncbi_tid)
             with open(os.path.join(output, 'capitalist', gg + '.fna'), 'a+') as output_fna:
                 header = 'ncbi_tid|%d|%s' % (ncbi_tid, title[1:])
                 output_fna.write('>%s\n%s\n' % (header, seq))
