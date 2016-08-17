c0590009d47e9acdd46b1ad949e0e719fe598aef
Benjamin Hillmann <bhillmann@outlook.com>
2016-08-17 14:15:20 -0500

    Replaced ; with .

--- ninja_shogun/scripts/shogun_capitalist_db.py
+++ ninja_shogun/scripts/shogun_capitalist_db.py
@@ -11,12 +11,10 @@ from ninja_dojo.taxonomy import NCBITree
 
 @click.command()
 @click.option('-i', '--input', type=click.File(), default='-')
-@click.option('-o', '--output', type=click.Path(), default=os.path.join(os.getcwd(), 'annotated'))
+@click.option('-o', '--output', type=click.Path(), default=os.path.join(os.getcwd(), 'capitalist'))
 @click.option('-x', '--extract_ncbi_tid', default='ncbi_tid|,|')
 def shogun_capitalist_db(input, output, extract_ncbi_tid):
-
     verify_make_dir(output)
-    verify_make_dir(os.path.join(output, 'capitalist'))
 
     begin, end = extract_ncbi_tid.split(',')
     tree = NCBITree()
@@ -27,8 +25,8 @@ def shogun_capitalist_db(input, output, extract_ncbi_tid):
         title.replace('\t', '|')
         ncbi_tid = int(find_between(title, begin, end))
         if ncbi_tid:
-            gg = tree.gg_lineage(ncbi_tid)
-            with open(os.path.join(output, 'capitalist', gg + '.fna'), 'a+') as output_fna:
+            gg = tree.gg_lineage(ncbi_tid).replace(';', '.')
+            with open(os.path.join(output, gg + '.fna'), 'a+') as output_fna:
                 header = 'ncbi_tid|%d|%s' % (ncbi_tid, title[1:])
                 output_fna.write('>%s\n%s\n' % (header, seq))
 
