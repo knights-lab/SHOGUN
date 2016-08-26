d59f312e3652e1608ed428912b585d44172e3366
Benjamin Hillmann <bhillmann@outlook.com>
2016-08-26 16:00:58 -0500

    Wrap keys call in list.

--- ninja_shogun/scripts/shogun_bt2_lca.py
+++ ninja_shogun/scripts/shogun_bt2_lca.py
@@ -30,12 +30,12 @@ def shogun_bt2_lca(input, output, bt2_indx, extract_ncbi_tid, depth, threads, an
     for fna_file in fna_files:
         sam_outf = os.path.join(output, '.'.join(str(os.path.basename(fna_file)).split('.')[:-1]) + '.sam')
         if os.path.isfile(sam_outf):
-            print("Found the Samfile %s. Skipping the alignment phase" % sam_outf)
+            print("Found the samfile <%s>. Skipping the alignment phase." % sam_outf)
         else:
             print(bowtie2_align(fna_file, sam_outf, bt2_indx, num_threads=threads))
 
     tree = NCBITree()
-    rank_name = tree.mp_ranks.keys()[depth]
+    rank_name = list(tree.mp_ranks.keys())[depth]
     if not rank_name:
         raise ValueError('Depth must be between 0 and 7, it was %d' % depth)        
 
