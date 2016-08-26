f8bbc09617276b489c02b15868da22764064f31e
Benjamin Hillmann <bhillmann@outlook.com>
2016-08-26 15:38:31 -0500

    Created the annotate module.

--- ninja_shogun/scripts/shogun_bt2_lca.py
+++ ninja_shogun/scripts/shogun_bt2_lca.py
@@ -3,7 +3,7 @@ import click
 from collections import Counter
 import os
 import pandas as pd
-from cytoolz import valmap
+from cytoolz import valmap, valfilter
 
 from ninja_utils.utils import find_between
 from ninja_utils.utils import verify_make_dir
@@ -21,7 +21,8 @@ from ninja_shogun.parsers import yield_alignments_from_sam_inf
 @click.option('-x', '--extract_ncbi_tid', default='ncbi_tid|,|', help='Characters that sandwich the NCBI TID in the reference FASTA (default="ncbi_tid|,|")')
 @click.option('-d', '--depth', type=click.INT, default=7, help='The depth of the search (7=species default, 0=No Collapse)')
 @click.option('-p', '--threads', type=click.INT, default=1, help='The number of threads to use (default=1)')
-def shogun_bt2_lca(input, output, bt2_indx, extract_ncbi_tid, depth, threads):
+@click.option('-a', '--annotate_lineage', type=click.BOOL, default=True, help='Annotate the NCBI Taxonomy ID with lineage (default=True)')
+def shogun_bt2_lca(input, output, bt2_indx, extract_ncbi_tid, depth, threads, annotate_lineage):
     verify_make_dir(output)
 
     fna_files = [os.path.join(input, filename) for filename in os.listdir(input) if filename.endswith('.fna')]
@@ -31,6 +32,10 @@ def shogun_bt2_lca(input, output, bt2_indx, extract_ncbi_tid, depth, threads):
         print(bowtie2_align(fna_file, sam_outf, bt2_indx, num_threads=threads))
 
     tree = NCBITree()
+    rank_name = tree.mp_ranks()[depth]
+    if not rank_name:
+        raise ValueError('Depth must be between 0 and 7, it was %d' % depth)        
+
     begin, end = extract_ncbi_tid.split(',')
 
     counts = []
@@ -48,8 +53,12 @@ def shogun_bt2_lca(input, output, bt2_indx, extract_ncbi_tid, depth, threads):
             else:
                 lca_map[qname] = ncbi_tid
 
-        lca_map = valmap(lambda x: tree.green_genes_lineage(x, depth=depth), lca_map)
-        taxon_counts = Counter(filter(None, lca_map.values()))
+        if annotate_lineage:
+            lca_map = valmap(lambda x: tree.green_genes_lineage(x, depth=depth), lca_map)
+            taxon_counts = Counter(filter(None, lca_map.values()))
+        else:
+            lca_map = valfilter(lambda x: tree.get_rank_from_taxon_id(x) == rank_name, lca_map)
+            taxon_counts = Counter(filter(None, lca_map.values()))
         counts.append(taxon_counts)
 
     df = pd.DataFrame(counts, index=['#' + '.'.join(os.path.basename(sample).split('.')[:-1]) for sample in sam_files])
