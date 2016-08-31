afdb6f1adb5eb1fbeebe32cb6e8681834129da59
Benjamin Hillmann <bhillmann@outlook.com>
2016-08-31 12:42:32 -0500

    Built the new build DB pipeline with seperate functions.

--- ninja_shogun/scripts/shogun_bt2_db.py
+++ ninja_shogun/scripts/shogun_bt2_db.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python
 import click
-import sys
 import os
 
-from ninja_utils.parsers import FASTA
-from ninja_utils.utils import find_between
 from ninja_utils.utils import verify_make_dir
 
-from ninja_dojo.database import RefSeqDatabase
-from ninja_dojo.taxonomy import NCBITree
+from ninja_dojo.scripts.annotate_fasta import annotate_fasta
 
 from ninja_shogun.wrappers import bowtie2_build
-
 
 @click.command()
 @click.option('-i', '--input', type=click.Path(), default='-', help='The input FASTA file for annotating with NCBI TID (default=stdin)')
@@ -20,44 +15,21 @@ from ninja_shogun.wrappers import bowtie2_build
 @click.option('--prefixes', default='*', help="Supply a comma-seperated list where the options are choices"
                                               " in ('AC', 'NC', 'NG', 'NM', 'NT', 'NW', 'NZ') e.g. NC,AC default=all")
 def shogun_bt2_db(input, output, extract_refseq_id, prefixes):
-    db = RefSeqDatabase()
-
-    verify_make_dir(output)
-    # check for the glob prefix
-    prefixes = prefixes.split(',')
-
-    begin, end = extract_refseq_id.split(',')
-    tree = NCBITree()
-
-    if '*' in prefixes:
-        prefix_set = set([_ for _ in db.refseq_prefix_mapper.keys()])
-    else:
-        prefix_set = set([_ for _ in prefixes])
-
+    # Verify the FASTA is annotated
     if input == '-':
         output_fn = 'stdin'
     else:
         output_fn = '.'.join(str(os.path.basename(input)).split('.')[:-1])
 
-    with open(input, 'r') if input != '-' else sys.stdin as inf:
-        with open(os.path.join(output, output_fn + '.annotated.fna'), 'w') as output_fna:
-            with open(os.path.join(output, output_fn + '.annotated.map'), 'w') as output_map:
-                inf_fasta = FASTA(inf)
-                for title, seq in inf_fasta.read():
-                    title = '>' + title
-                    title.replace('\t', '|')
-                    refseq_accession_version = find_between(title, begin, end)
-                    if refseq_accession_version[:2] in prefix_set:
-                        ncbi_tid = db.get_ncbi_tid_from_refseq_accession_version(refseq_accession_version)
-                        if ncbi_tid:
-                            gg = tree.green_genes_lineage(ncbi_tid[0])
-                            gg = '; '.join(gg.split(';'))
-                            header = 'ncbi_tid|%d|%s' % (ncbi_tid[0], title[1:])
-                            output_fna.write('>%s\n%s\n' % (header, seq))
-                            output_map.write('%s\t%s\n' % (header.split()[0], gg))
+    outf_fasta = os.path.join(output, output_fn + '.annotated.fna')
+    if not os.path.isfile(outf_fasta):
+        annotate_fasta(input, output, extract_refseq_id, prefixes)
+    else:
+        print("Found the output file \"%s\". Skipping the annotation phase for this file." % outf_fasta)
 
+    # Build the output BT2 database
     verify_make_dir(os.path.join(output, 'bt2'))
-    print(bowtie2_build(os.path.join(output, output_fn + '.annotated.fna'), os.path.join(output, 'bt2', output_fn)))
+    print(bowtie2_build(outf_fasta, os.path.join(output, 'bt2', output_fn)))
 
 if __name__ == '__main__':
     shogun_bt2_db()
--- ninja_shogun/scripts/shogun_utree_db.py
+++ ninja_shogun/scripts/shogun_utree_db.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
 import click
-import sys
 import os
 
-from ninja_utils.parsers import FASTA
-from ninja_utils.utils import find_between
 from ninja_utils.utils import verify_make_dir
 
-from ninja_dojo.database import RefSeqDatabase
-from ninja_dojo.taxonomy import NCBITree
+from ninja_dojo.scripts.annotate_fasta import annotate_fasta
 
 from ninja_shogun.wrappers import utree_build, utree_compress
 from ninja_shogun import SETTINGS
@@ -22,48 +18,20 @@ from ninja_shogun import SETTINGS
 @click.option('--prefixes', default='*', help="Supply a comma-seperated list where the options are choices"
                                               " in ('AC', 'NC', 'NG', 'NM', 'NT', 'NW', 'NZ') e.g. NC,AC default=all")
 def shogun_utree_db(input, output, extract_refseq_id, threads, prefixes):
-    db = RefSeqDatabase()
-
-    verify_make_dir(output)
-    # check for the glob prefix
-    prefixes = prefixes.split(',')
-
-    begin, end = extract_refseq_id.split(',')
-    tree = NCBITree()
-
-    if '*' in prefixes:
-        prefix_set = set([_ for _ in db.refseq_prefix_mapper.keys()])
-    else:
-        prefix_set = set([_ for _ in prefixes])
-
+    # Verify the FASTA is annotated
     if input == '-':
         output_fn = 'stdin'
     else:
         output_fn = '.'.join(str(os.path.basename(input)).split('.')[:-1])
 
-    path_annotated_fasta = os.path.join(output, output_fn + '.annotated.fna')
-    path_annotated_map = os.path.join(output, output_fn + '.annotated.map')
-
-    if os.path.exists(path_annotated_fasta) and os.path.exists(path_annotated_map):
-        print('Annotated FASTA and mapping file %s and %s exist, skipping the annotation step.' % (path_annotated_fasta, path_annotated_map))
+    outf_fasta = os.path.join(output, output_fn + '.annotated.fna')
+    outf_map = os.path.join(output, output_fn, + '.annotated.map')
+    if not os.path.isfile(outf_fasta) and not os.path.isfile(outf_map):
+        annotate_fasta(input, output, extract_refseq_id, prefixes)
     else:
-        with open(input, 'r') if input != '-' else sys.stdin as inf:
-            with open(path_annotated_fasta, 'w') as output_fna:
-                with open(path_annotated_map, 'w') as output_map:
-                    inf_fasta = FASTA(inf)
-                    for title, seq in inf_fasta.read():
-                        title = '>' + title
-                        title.replace('\t', '|')
-                        refseq_accession_version = find_between(title, begin, end)
-                        if refseq_accession_version[:2] in prefix_set:
-                            ncbi_tid = db.get_ncbi_tid_from_refseq_accession_version(refseq_accession_version)
-                            if ncbi_tid:
-                                gg = tree.green_genes_lineage(ncbi_tid[0])
-                                gg = '; '.join(gg.split(';'))
-                                header = 'ncbi_tid|%d|%s' % (ncbi_tid[0], title[1:])
-                                output_fna.write('>%s\n%s\n' % (header, seq))
-                                output_map.write('%s\t%s\n' % (header.split()[0], gg))
+        print("Found the output file \"%s\". Skipping the annotation phase for this file." % outf_fasta)
 
+    # Build the output CTR
     verify_make_dir(os.path.join(output, 'utree'))
     path_uncompressed_tree = os.path.join(output, 'utree', output_fn + '.utr')
     path_compressed_tree = os.path.join(output, 'utree', output_fn+'.ctr')
@@ -71,7 +39,7 @@ def shogun_utree_db(input, output, extract_refseq_id, threads, prefixes):
         print('Compressed tree database file %s exists, skipping this step.' % path_compressed_tree)
     else:
         if not os.path.exists(path_uncompressed_tree):
-            print(utree_build(path_annotated_fasta, path_annotated_map, path_uncompressed_tree, threads=threads))
+            print(utree_build(outf_fasta, outf_map, path_uncompressed_tree, threads=threads))
         print(utree_compress(path_uncompressed_tree, path_compressed_tree))
         os.remove(path_uncompressed_tree)
 
