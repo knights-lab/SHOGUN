9f5beaad8a04aaefdb7225f0693dade9ea84ffda
Benjamin Hillmann <bhillmann@outlook.com>
2016-08-31 12:05:41 -0500

    Added the EMBALMBER outf.

--- ninja_shogun/scripts/shogun_utree_capitalist.py
+++ ninja_shogun/scripts/shogun_utree_capitalist.py
@@ -16,7 +16,7 @@ from ninja_shogun.wrappers import utree_search, embalmer_align
 
 @click.command()
 @click.option('-i', '--input', type=click.Path(), default=os.getcwd(), help='Directory containing the input FASTA files with ".fna" extensions (default=cwd)')
-@click.option('-o', '--output', type=click.Path(), default=os.getcwd(), help='Output directory for the results')
+@click.option('-o', '--output', type=click.Path(), default=os.path.join(os.getcwd(), 'shogun_utree_capitalist_out'), help='Output directory for the results')
 @click.option('-u', '--utree_indx', required=True, help='Path to the bowtie2 index')
 @click.option('-r', '--reference_fasta', required=True, help='Path to the annotated Reference FASTA file with ".fna" extension')
 @click.option('-x', '--extract_ncbi_tid', default='ncbi_tid|,|', help='Characters that sandwich the NCBI TID in the reference FASTA (default="ncbi_tid|,|")')
@@ -28,67 +28,78 @@ def shogun_utree_capitalist(input, output, utree_indx, reference_fasta, extract_
 
     for basename in basenames:
         fna_file = os.path.join(input, basename + '.fna')
-        tsv_outf = os.path.join(output, basename + 'utree.tsv')
-        print(utree_search(utree_indx, fna_file, tsv_outf))
+        tsv_outf = os.path.join(output, basename + '.utree.tsv')
+        if not os.path.isfile(tsv_outf):
+            print(utree_search(utree_indx, fna_file, tsv_outf))
+        else:
+            print("Found the output file \"%s\". Skipping the alignment phase for this file." % tsv_outf)
 
-    lca_maps = defaultdict(lambda: defaultdict(list))
-    for basename in basenames:
-        utree_tsv = os.path.join(output, basename + '.utree.tsv')
-        with open(utree_tsv) as inf:
-            tsv_parser = csv.reader(inf, delimiter='\t')
-            for line in tsv_parser:
-                if line[1]:
-                    lca_maps[';'.join(line[1].split('; '))][basename].append(line[0])
+    embalmer_outf = os.path.join(output, 'embalmer_out.txt')
+    # Indexing for emblalmer
+    if not os.path.isfile(embalmer_outf):
+        lca_maps = defaultdict(lambda: defaultdict(list))
+        for basename in basenames:
+            utree_tsv = os.path.join(output, basename + '.utree.tsv')
+            with open(utree_tsv) as inf:
+                tsv_parser = csv.reader(inf, delimiter='\t')
+                for line in tsv_parser:
+                    if line[1]:
+                        lca_maps[';'.join(line[1].split('; '))][basename].append(line[0])
 
-    fna_faidx = {}
-    for basename in basenames:
-        fna_faidx[basename] = pyfaidx.Fasta(os.path.join(input, basename + '.fna'))
 
-    reference_map = defaultdict(list)
-    with open('.'.join(os.path.basename(reference_fasta).split('.')[:-1]) + '.map') as inf:
-        tsv_in = csv.reader(inf, delimiter='\t')
-        for line in tsv_in:
-            reference_map[';'.join(line[1].split('; '))].append(line[0])
+        fna_faidx = {}
+        for basename in basenames:
+            fna_faidx[basename] = pyfaidx.Fasta(os.path.join(input, basename + '.fna'))
 
-    # reverse the dict to feed into embalmer
-    references_faidx = pyfaidx.Fasta(reference_fasta)
+        reference_map = defaultdict(list)
+        with open('.'.join(os.path.basename(reference_fasta).split('.')[:-1]) + '.map') as inf:
+            tsv_in = csv.reader(inf, delimiter='\t')
+            for line in tsv_in:
+                reference_map[';'.join(line[1].split('; '))].append(line[0])
 
-    tmpdir = tempfile.mkdtemp()
-    with open(os.path.join(output, 'embalmer_out.txt'), 'w') as embalmer_cat:
-        for species in lca_maps.keys():
+        # reverse the dict to feed into embalmer
+        references_faidx = pyfaidx.Fasta(reference_fasta)
 
-            queries_fna_filename = os.path.join(tmpdir, 'queries.fna')
-            references_fna_filename = os.path.join(tmpdir, 'reference.fna')
-            output_filename = os.path.join(tmpdir, 'output.txt')
+        tmpdir = tempfile.mkdtemp()
+        with open(embalmer_outf, 'w') as embalmer_cat:
+            for species in lca_maps.keys():
 
-            with open(queries_fna_filename, 'w') as queries_fna:
-                for basename in lca_maps[species].keys():
-                    for header in lca_maps[species][basename]:
-                        record = fna_faidx[basename][header][:]
-                        queries_fna.write('>%s\n%s\n' % (record.name, record.seq))
+                queries_fna_filename = os.path.join(tmpdir, 'queries.fna')
+                references_fna_filename = os.path.join(tmpdir, 'reference.fna')
+                output_filename = os.path.join(tmpdir, 'output.txt')
 
-            with open(references_fna_filename, 'w') as references_fna:
-                for i in reference_map[species]:
-                        record = references_faidx[i][:]
-                        references_fna.write('>%s\n%s\n' % (record.name, record.seq))
+                with open(queries_fna_filename, 'w') as queries_fna:
+                    for basename in lca_maps[species].keys():
+                        for header in lca_maps[species][basename]:
+                            record = fna_faidx[basename][header][:]
+                            queries_fna.write('>%s\n%s\n' % (record.name, record.seq))
 
-            embalmer_align(queries_fna_filename, references_fna_filename, output_filename)
+                with open(references_fna_filename, 'w') as references_fna:
+                    for i in reference_map[species]:
+                            record = references_faidx[i][:]
+                            references_fna.write('>%s\n%s\n' % (record.name, record.seq))
 
-            with open(output_filename) as embalmer_out:
-                for line in embalmer_out:
-                    embalmer_cat.write(line)
+                embalmer_align(queries_fna_filename, references_fna_filename, output_filename)
 
-            os.remove(queries_fna_filename)
-            os.remove(references_fna_filename)
-            os.remove(output_filename)
+                with open(output_filename) as embalmer_out:
+                    for line in embalmer_out:
+                        embalmer_cat.write(line)
 
-    os.rmdir(tmpdir)
+                os.remove(queries_fna_filename)
+                os.remove(references_fna_filename)
+                os.remove(output_filename)
 
+        os.rmdir(tmpdir)
+    else:
+        print("Found the output file \"%s\". Skipping the strain alignment phase for this file." % embalmer_outf)
+
+
+    # Convert the results from embalmer into CSV
     sparse_ncbi_dict = defaultdict(dict)
 
     begin, end = extract_ncbi_tid.split(',')
     # build query by NCBI_TID DataFrame
-    with open(os.path.join(output, 'embalmer_out.txt')) as embalmer_cat:
+    with open(embalmer_outf) as embalmer_cat:
         embalmer_csv = csv.reader(embalmer_cat, delimiter='\t')
         for line in embalmer_csv:
             # line[0] = qname, line[1] = rname, line[2] = %match
