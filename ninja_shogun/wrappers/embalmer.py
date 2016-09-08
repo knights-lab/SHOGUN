46a9bb628bc907bd927ea65199998fe1389cff14
Benjamin Hillmann <bhillmann@outlook.com>
2016-09-08 15:42:41 -0500

    Updated the embalmer.

--- ninja_shogun/wrappers/embalmer.py
+++ ninja_shogun/wrappers/embalmer.py
@@ -3,13 +3,16 @@ from ninja_utils.utils import run_command
 from .. import SETTINGS
 
 
-def embalmer_align(queries, references, outfile, mode='CAPITALIST', num_threads=SETTINGS.N_jobs, shell=False):
-    cmd = ['embalmer',
-           '--queries', queries,
-           '--references', references,
-           '--output', outfile,
-           '--threads', str(num_threads),
-           '--mode', mode,
-           '--shear',
-           ]
+def embalmer_align(queries, references, outfile, mode='CAPITALIST2', num_threads=SETTINGS.N_jobs, shell=False):
+    cmd = [
+        'embalmer',
+        '--queries', queries,
+        '--references', references,
+        '--output', outfile,
+        '--threads', str(num_threads),
+        '--mode', mode,
+        '--shear',
+        '--latency', 64,
+        '--id', .985,
+    ]
     return run_command(cmd, shell=shell)
