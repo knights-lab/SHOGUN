583007532626f2c5b5dd1ed316abef087c2412ae
Benjamin Hillmann <bhillmann@outlook.com>
2016-09-09 13:48:32 -0500

    Added the filenames.

--- ninja_shogun/scripts/shogun_bt2_db.py
+++ ninja_shogun/scripts/shogun_bt2_db.py
@@ -5,21 +5,23 @@ import os
 from ninja_utils.utils import verify_make_dir
 from ninja_utils.parsers import FASTA
 
-from ninja_dojo.annotaters.refseq import refseq_annotater
+from ninja_dojo.annotaters import GIAnnotater, RefSeqAnnotater
 
 from ninja_shogun.wrappers import bowtie2_build
 
 @click.command()
 @click.option('-i', '--input', type=click.Path(), default='-', help='The input FASTA file for annotating with NCBI TID (default=stdin)')
 @click.option('-o', '--output', type=click.Path(), default=os.path.join(os.getcwd(), 'annotated'), help='The directory to output the formatted DB and BT2 db (default=annotated)')
-@click.option('-x', '--extract_refseq_id', default='ref|,|', help='Characters that sandwich the RefSeq Accession Version in the reference FASTA (default="ref|,|")')
+@click.option('-a', '-annotater', type=click.Choice(['gi', 'refseq']), default='refseq', help='The annotater to use.',
+              show_default=True)
+@click.option('-x', '--extract_id', default='ref|,|',
+              help='Characters that sandwich the RefSeq Accession Version in the reference FASTA', show_default=True)
 @click.option('--prefixes', default='*', help="Supply a comma-seperated list where the options are choices"
                                               " in ('AC', 'NC', 'NG', 'NM', 'NT', 'NW', 'NZ') e.g. NC,AC default=all")
 @click.option('-d', '--depth', default=7, help="The depth to annotate the map")
 @click.option('-f', '--depth-force', default=True, help="Force the depth criterion if missing annotation")
-def shogun_bt2_db(input, output, extract_refseq_id, prefixes, depth, depth_force):
+def shogun_bt2_db(input, output, extract_id, prefixes, depth, depth_force):
     verify_make_dir(output)
-
     # Verify the FASTA is annotated
     if input == '-':
         output_fn = 'stdin'
@@ -29,16 +31,23 @@ def shogun_bt2_db(input, output, extract_refseq_id, prefixes, depth, depth_force
     outf_fasta = os.path.join(output, output_fn + '.annotated.fna')
     outf_map = os.path.join(output, output_fn + '.annotated.map')
     if not os.path.isfile(outf_fasta) or not os.path.isfile(outf_map):
+        tree = NCBITree()
+        db = RefSeqDatabase()
+        if annotater == 'refseq':
+            annotater_class = RefSeqAnnotater(extract_id, prefixes, db, tree, depth=depth, depth_force=depth_force)
+        else:
+            annotater_class = GIAnnotater(extract_id, db, tree, depth=depth, depth_force=depth_force)
+
         with open(outf_fasta, 'w') as output_fna:
             with open(outf_map, 'w') as output_map:
                 with open(input) as inf:
                     inf_fasta = FASTA(inf)
-                    annotater = refseq_annotater(inf_fasta.read(), prefixes, extract_refseq_id, depth=depth, depth_force=depth_force)
-                    for lines_fna, lines_map in anno:
+                    for lines_fna, lines_map in annotater_class.annotate(inf_fasta.read()):
                         output_fna.write(lines_fna)
                         output_map.write(lines_map)
     else:
-        print("Found the output files \"%s\" and \"%s\". Skipping the annotation phase for this file." % (outf_fasta, outf_map))
+        print("Found the output files \"%s\" and \"%s\". Skipping the annotation phase for this file." % (
+            outf_fasta, outf_map))
 
     # Build the output BT2 database
     verify_make_dir(os.path.join(output, 'bt2'))
