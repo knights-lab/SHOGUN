218cf0f32daaa488ec02e7c7c3d061e9c0e1ff6f
Benjamin Hillmann <bhillmann@outlook.com>
2016-09-14 11:09:53 -0500

    Proper naming of the output file.

--- ninja_shogun/scripts/shogun_utree_lca.py
+++ ninja_shogun/scripts/shogun_utree_lca.py
@@ -12,7 +12,7 @@ from ninja_shogun.wrappers import utree_search
 
 @click.command()
 @click.option('-i', '--input', type=click.Path(), default=os.getcwd(), help='Directory containing the input FASTA files with ".fna" extensions (default=cwd)')
-@click.option('-o', '--output', type=click.Path(), default=os.path.join(os.getcwd(), 'shogun_utree_capitalist_out'), help='Output directory for the results')
+@click.option('-o', '--output', type=click.Path(), default=os.path.join(os.getcwd(), 'shogun_utree_lca_out'), help='Output directory for the results')
 @click.option('-u', '--utree_indx', required=True, help='Path to the bowtie2 index')
 @click.option('-p', '--threads', type=click.INT, default=1, help='The number of threads to use (default=1)')
 def shogun_utree_lca(input, output, utree_indx, threads):
