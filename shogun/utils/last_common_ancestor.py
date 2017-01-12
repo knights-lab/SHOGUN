61b5bd486cedb36fb068e9f02a8910b4cce4b5f0
Benjamin Hillmann <bhillmann@outlook.com>
2017-01-12 16:08:17 -0600

    Updated the README.md

---
+++ shogun/utils/last_common_ancestor.py
@@ -0,0 +1,24 @@
+from find_betweens import verify_make_dir
+
+from shogun.parsers import yield_alignments_from_sam_inf
+
+
+def build_lca_map(sam_inf, extract_ncbi_tid, tree):
+    """
+    Build a last common ancestor dictionary
+    :param sam_inf: path to SAM infile
+    :param extract_ncbi_tid: function to extract ncbi_tid
+    :param tree: NCBITree
+    :return: dict key (query name: string) value (ncbi_tid: int)
+    """
+    lca_map = {}
+    for qname, rname in yield_alignments_from_sam_inf(sam_inf):
+        ncbi_tid = extract_ncbi_tid(rname)
+        if qname in lca_map:
+            current_ncbi_tid = lca_map[qname]
+            if current_ncbi_tid:
+                if current_ncbi_tid != ncbi_tid:
+                    lca_map[qname] = tree.lowest_common_ancestor(ncbi_tid, current_ncbi_tid)
+        else:
+            lca_map[qname] = ncbi_tid
+    return lca_map
