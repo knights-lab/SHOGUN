4f556a284ab736e82799296598c43de903ac57fb
Benjamin Hillmann <bhillmann@outlook.com>
2017-01-15 12:19:11 -0600

    Added the zipped reader.

--- shogun/scripts/shogun_utree_functional.py
+++ shogun/scripts/shogun_utree_functional.py
@@ -1,10 +1,10 @@
 """
-Functional repertiore prediction with UTree Species/Strain Classification
+functional repertiore prediction with utree species/strain classification
 """
 
 import click
 import os
-from collections import Counter
+from collections import counter
 import csv
 import pandas as pd
 
@@ -14,39 +14,43 @@ from shogun.wrappers import utree_search
 
 
 @click.command()
-@click.option('-i', '--input', type=click.Path(), default=os.getcwd(), help='Directory containing the input FASTA files with ".fna" extensions (default=cwd)')
-@click.option('-o', '--output', type=click.Path(), default=os.path.join(os.getcwd(), 'shogun_utree_lca_out'), help='Output directory for the results')
-@click.option('-u', '--utree_indx', required=True, help='Path to the bowtie2 index')
-@click.option('-p', '--threads', type=click.INT, default=1, help='The number of threads to use (default=1)')
+@click.option('-i', '--input', type=click.path(), default=os.getcwd(), help='directory containing the input fasta files with ".fna" extensions (default=cwd)')
+@click.option('-o', '--output', type=click.path(), default=os.path.join(os.getcwd(), 'shogun_utree_lca_out'), help='output directory for the results')
+@click.option('-u', '--utree_indx', required=true, help='path to the bowtie2 index')
+@click.option('-p', '--threads', type=click.int, default=1, help='the number of threads to use (default=1)')
 def shogun_utree_functional(input, output, utree_indx, threads):
     verify_make_dir(output)
 
     basenames = [os.path.basename(filename)[:-4] for filename in os.listdir(input) if filename.endswith('.fna')]
 
+    # Run the species pipeline
     for basename in basenames:
         fna_file = os.path.join(input, basename + '.fna')
-        tsv_outf = os.path.join(output, basename + 'species.utree.tsv')
+        tsv_outf = os.path.join(output, basename + '.species.utree.tsv')
         if not os.path.isfile(tsv_outf):
             print(utree_search(utree_indx, fna_file, tsv_outf))
         else:
-            print("Found the output file \"%s\". Skipping the alignment phase for this file." % tsv_outf)
+            print("found the output file \"%s\". skipping the species alignment phase for this file." % tsv_outf)
 
-    counts = []
-    utree_species_outf = os.path.join(output, 'taxon_counts.txt')
-    # Indexing for emblalmer
-    if not os.path.isfile(utree_species_outf):
-        for basename in basenames:
-            lcas = []
-            utree_tsv = os.path.join(output, basename + '.species.utree.tsv')
-            with open(utree_tsv) as inf:
-                tsv_parser = csv.reader(inf, delimiter='\t')
-                for line in tsv_parser:
-                    if line[1]:
-                        lcas.append(';'.join(line[1].split('; ')))
-            counts.append(Counter(filter(None, lcas)))
+    # Run the strain pipeline
+    for basename in basenames:
+        fna_file = os.path.join(input, basename + '.fna')
+        tsv_outf = os.path.join(output, basename + '.strain.utree.tsv')
+        if not os.path.isfile(tsv_outf):
+            print(utree_search(utree_indx, fna_file, tsv_outf))
+        else:
+            print("found the output file \"%s\". skipping the strain alignment phase for this file." % tsv_outf)
 
-    species_df = pd.DataFrame(counts, index=basenames)
-    species_df.T.to_csv(utree_species_outf, sep='\t')
+    for basename in basenames:
+        species_tsv = os.path.join(output, basename + '.species.utree.tsv')
+        strain_tsv = os.path.join(output, basename + '.strain.utree.tsv')
+        with open(species_tsv) as species_inf:
+            species_reader = csv.reader(species_inf, delimiter="\t")
+            with open(strain_tsv) as strain_inf:
+                strain_reader = csv.reader(strain_inf, delimiter="\t")
+                for species, strain in zip(species_reader, strain_reader):
+                    print(species)
+                    print(strain)
 
 if __name__ == '__main__':
     shogun_utree_functional()
