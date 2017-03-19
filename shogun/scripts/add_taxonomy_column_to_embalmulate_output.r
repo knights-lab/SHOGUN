998932fda9a6a4f77325863f6316b897ae5c0638
danknights <dknights@umn.edu>
2017-03-18 23:23:18 -0500

    now uses ref2tax map

--- shogun/scripts/add_taxonomy_column_to_embalmulate_output.r
+++ shogun/scripts/add_taxonomy_column_to_embalmulate_output.r
@@ -1,9 +1,11 @@
 #!/usr/bin/env Rscript
 # usage
-# Rscript *.r embalmer_taxon_table.tsv > taxon_table_with_taxonomy_column.tsv
+# Rscript *.r embalmer_taxon_table.tsv ref2taxa.txt > taxon_table_with_taxonomy_column.tsv
 args <- commandArgs(trailing=TRUE)
+ref2taxa <- read.table(args[2],sep='\t',head=T,row=1,check=F,comment='',quote='')
 x <- read.table(args[1],sep='\t',head=T,check=F,row=1,comment='',quote='')
-x$taxonomy <- rownames(x)
+print(mean(rownames(x) %in% rownames(ref2taxa)))
+x$taxonomy <- ref2taxa[match(rownames(x),rownames(ref2taxa)),1]
 sink(args[1])
 cat('#Taxon ID\t')
 write.table(x,sep='\t',quote=F)
