3edcd313bccf46bacb2debd43839b450f5b1ab39
Benjamin Hillmann <Benjamin Hillmann>
2017-06-19 16:25:23 -0500

    Updated the database.

--- shogun/aligners/__init__.py
+++ shogun/aligners/__init__.py
@@ -5,12 +5,17 @@ This software is released under the GNU Affero General Public License (AGPL) v3.
 """
 
 import os
+from yaml import load
 
+from shogun.wrappers import embalmer_align
 
 class Aligner:
-    def __init__(self, database_dir):
-        check, msg = self.check_database(database_dir)
+    _name = None
 
+    def __init__(self, database_dir, name):
+        check, msg = self.check_database(database_dir, name)
+
+        self.data_files = load(os.path.join(database_dir, 'metadata.yaml'))
         if not check:
             raise Exception("Database %s is not formatted correctly: %s" % (database_dir, msg))
 
@@ -18,24 +23,28 @@ class Aligner:
 
     @classmethod
     def check_database(cls, dir):
+        stream = open(os.path.join(dir, 'metadata.yaml'))
+        data_files = load(stream)
+        for key, value in data_files[cls._name].items():
+            if value:
+                if not os.path.exists(os.path.join(dir, value)):
+                    return False, '%s not found' % (os.path.join(dir, value))
+        return True, ''
+
+    def align(self):
         raise NotImplementedError
 
-
 class EmbalmerAligner(Aligner):
+    _name = 'embalmer'
+
     def __init__(self, database_dir):
         super().__init__(database_dir)
 
-    @classmethod
-    def check_database(cls, dir):
-        if not os.path.exists(dir):
-            return False, "Directory does not exist"
-        if not os.path.exists(os.path.join(dir, 'embalmer')):
-            return False, "embalmer subdir does not exist"
-        else:
-            for file in os.listdir(os.path.join(dir, "embalmer")):
-                if file.split()[-1] == 'edb':
-                    return True
-        return False, 'No edb file found'
+    def align(self, infile, outfile):
+        embalmer_align(infile, outfile,
+            self.database, tax=self.tax, accelerator=self.accelerator, shell=self.shell, taxa_ncbi=self.taxa_ncbi)
+
+
 
 
 class UtreeAligner(Aligner):
