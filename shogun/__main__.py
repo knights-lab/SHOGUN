df97b8f68a33845dcd21556b8b811b0199932d9c
bhillmann <bhillmann@outlook.com>
2017-08-24 12:25:12 -0500

    Added -h.

--- shogun/__main__.py
+++ shogun/__main__.py
@@ -33,7 +33,8 @@ TAXAMAP = dict(zip(TAXA, range(1, 9)))
 @click.group(invoke_without_command=False, help=ROOT_COMMAND_HELP)
 @click.option('--log', type=click.Choice(('debug', 'info', 'warning', 'critical')), help="The log level to record.", default="warning")
 @click.option('--shell/--no-shell', default=False, help="Use the shell for Python subcommands (not recommended).")
-@click.version_option(version=__version__)
+@click.version_option('-v', '--version', version=__version__)
+@click.help_option('-h', '--help')
 @click.pass_context
 def cli(ctx, log, shell):
     ctx.obj = {'log': log}
