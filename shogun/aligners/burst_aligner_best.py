2d98e82af409979ac488a82b546a8b1b3f2d3016
bhillmann <bhillmann@outlook.com>
2017-09-07 23:35:59 -0500

    Updated the alignment file.

--- shogun/aligners/burst_aligner_best.py
+++ shogun/aligners/burst_aligner_best.py
@@ -10,6 +10,8 @@ from .burst_aligner import  BurstAligner
 
 from shogun.wrappers import burst_align_best
 from shogun.utils import read_fasta
+from shogun import logger
+
 
 class BurstAlignerBest(BurstAligner):
     _name = 'burst_best'
@@ -23,25 +25,30 @@ class BurstAlignerBest(BurstAligner):
         #TODO: pie chart and coverage
         proc, out, err = burst_align_best(infile, self.outfile, self.database, accelerator=self.accelerator, shell=self.shell, threads=self.threads)
         if self.post_align:
-            post_align = self._post_align(infile, self.outfile)
+            alignments = self._post_align(self.outfile)
             post_filtered = os.path.join(outdir, 'combined_seqs.filtered.fna')
             post_unfiltered = os.path.join(outdir, 'combined_seqs.fna')
-            with open(post_filtered, 'w') as outf_post_filtered:
-                with open(post_unfiltered, 'w') as outf_post_unfiltered:
-                    for title, seq, alignment_score in post_align:
-                        if alignment_score >= self.percent_id:
-                            outf_post_filtered.write(">%s\n%s\n" % (title, seq))
-                        else:
-                            outf_post_unfiltered.write(">%s\n%s\n" % (title, seq))
+            with open(infile) as input_fasta:
+                fasta_gen = read_fasta(input_fasta)
+                with open(post_filtered, 'w') as outf_post_filtered:
+                    with open(post_unfiltered, 'w') as outf_post_unfiltered:
+                        for title, seq in fasta_gen:
+                            if title in alignments:
+                                outf_post_filtered.write(">%s\n%s\n" % (title, seq))
+                            else:
+                                outf_post_unfiltered.write(">%s\n%s\n" % (title, seq))
         return proc, out, err
 
-    def _post_align(self, inf, outf):
-       with open(outf) as alignment_file:
-           alignment_gen = csv.reader(alignment_file, delimiter="\t")
-           with open(inf) as fasta_infile:
-               fasta_gen = read_fasta(fasta_infile)
-               for row, (title, seq) in zip(alignment_gen, fasta_gen):
-                   alignment_score = float(row[2])
-                   yield title, seq, alignment_score
-
+    def _post_align(self, outf):
+        alignments = set()
+        i = 0
+        with open(outf) as alignment_file:
+            alignment_gen = csv.reader(alignment_file, delimiter="\t")
+            for row in alignment_gen:
+                alignment_score = float(row[2])
+                if alignment_score > self.percent_id:
+                    alignments.add([row[0]])
+                    i += 1
+        logger.info("Human hits filter: %d" % i)
+        return alignments
 
